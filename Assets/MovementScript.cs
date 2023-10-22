377e496765bb485f76b304e4913bcfb713e012a8
Leo Wang <lzw12@scasd.org>
2023-10-21 22:57:17 -0400

    initial commit

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Vector2 movementSpeed = new Vector2(100f, 100f);
+    private new Rigidbody2D _rigidbody2D;
+    private Vector2 _input = new Vector2(0f, 0f);
+    private void Awake()
+    {
+        _rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+
+        _rigidbody2D.angularDrag = 0f;
+        _rigidbody2D.gravityScale = 0f;
+    }
+
+    private void Update()
+    {
+        _input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _input * (movementSpeed * Time.fixedDeltaTime));
+    }
+}
