6b611396b5854cb8fd548196ba099669213abfd6
Leo Wang <lzw12@scasd.org>
2023-10-22 21:48:08 -0400

    player script + input system

--- Assets/_Scripts/MovementScript.cs
+++ Assets/_Scripts/MovementScript.cs
@@ -2,27 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
-    public Vector2 movementSpeed = new Vector2(7f, 7f);
-    private new Rigidbody2D _rigidbody2D;
-    private Vector2 _input = new Vector2(0f, 0f);
+    public float moveSpeed = 150f;
+    public float maxSpeed = 8f;
+    public float idleFriction = 0.9f; // % of speed deleted from velocity
+    private new Rigidbody2D _rb;
+    private Vector2 _input = Vector2.zero;
+    private SpriteRenderer _sr;
     private void Awake()
     {
-        _rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
-
-        _rigidbody2D.angularDrag = 0f;
-        _rigidbody2D.gravityScale = 0f;
+        _rb = gameObject.AddComponent<Rigidbody2D>();
+        _sr = gameObject.AddComponent<SpriteRenderer>();
+        _rb.angularDrag = 0f;
+        _rb.gravityScale = 0f;
     }
-
-    private void Update()
-    {
-        _input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-    }
-
+    
     private void FixedUpdate()
     {
-        _rigidbody2D.MovePosition(_rigidbody2D.position + _input * (movementSpeed * Time.fixedDeltaTime));
+        if (_input != Vector2.zero)
+        {
+            _rb.velocity = Vector2.ClampMagnitude(_rb.velocity + (_input * (moveSpeed * Time.deltaTime)), maxSpeed);
+
+            if (_input.x > 0)
+            {
+                _sr.flipX = false;
+            }
+            else if(_input.x < 0)
+            {
+                _sr.flipX = true;
+            }
+        }
+        else
+        {
+            _rb.velocity = Vector2.Lerp(_rb.velocity, Vector2.zero, idleFriction);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        _input = value.Get<Vector2>();
     }
 }
