9a1fc98749519c8512769d073429eff0f549b915
Joshua Lin <linzipper17@gmail.com>
2023-10-26 16:35:55 -0400

    added HealthBar

---
+++ Assets/HealthBarControler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBarControler : MonoBehaviour
+{
+    public Image HealthBar;
+    public float healthAmount = 100f;
+    public TMP_Text healthText;
+
+
+   private void Update()
+    {
+       /* if (healthAmount <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+
+        }
+*/
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(20);
+        }
+
+        healthText.text = "Health: " + healthAmount;
+    }
+    
+    public void TakeDamage(float Damage)
+    {
+        healthAmount -= Damage;
+        HealthBar.fillAmount = healthAmount / 100;
+
+    }
+
+    /*public void HealingBlue(float healPoints)
+    {
+        healthAmount += healPoints;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        HBB.fillAmount = healthAmount / 100;
+    }
+*/
+}
