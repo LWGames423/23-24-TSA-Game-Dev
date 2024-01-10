52273208d59eb5859c4bce7af3baab2c3ef2e3a2
Leo Wang <71103552+LWGames423@users.noreply.github.com>
2024-01-10 13:23:09 -0500

    stamina regan

--- Assets/_Scripts/StaminaController.cs
+++ Assets/_Scripts/StaminaController.cs
@@ -13,11 +13,11 @@ public class StaminaController : MonoBehaviour
     [FormerlySerializedAs("stamAmount")] public float maxStam = 100f;
     public float currentStam;
     public TMP_Text stamText;
-    
+
     public float regenRate = 2f;
     private float _elapsed;
     public float regenDelay;
-    
+
     private void Awake()
     {
         currentStam = maxStam;
@@ -29,6 +29,7 @@ public class StaminaController : MonoBehaviour
     {
         RegenStamina();
         stamText.text = "Stamina: " + Decimal.Round((decimal)currentStam, 0);
+        stamBar.fillAmount = currentStam / 100;
     }
 
     public void RegenStamina()
@@ -36,15 +37,14 @@ public class StaminaController : MonoBehaviour
         if (_elapsed > regenDelay)
         {
             currentStam = Mathf.Min(currentStam + regenRate * Time.deltaTime, maxStam);
-        }    
+        }
         _elapsed += Time.deltaTime;
 
     }
-    
+
     public void LoseStamina(float stamina)
     {
         _elapsed = 0f;
         currentStam = Mathf.Max(currentStam - stamina, 0);
-        stamBar.fillAmount = currentStam / 100;
     }
 }
