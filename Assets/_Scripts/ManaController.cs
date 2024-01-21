2248162122c86c9285662c3025c37d9ea32264bb
Leo <lwgames0423@gmail.com>
2024-01-20 20:49:18 -0500

    mana actually shows up in bar now

--- Assets/_Scripts/ManaController.cs
+++ Assets/_Scripts/ManaController.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class ManaController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [FormerlySerializedAs("stamBar")] public Image manaBar;
+    [FormerlySerializedAs("maxStam")] public float maxMana = 100f;
+    [FormerlySerializedAs("currentStam")] public float currentMana;
+    [FormerlySerializedAs("stamText")] public TMP_Text manaText;
+
+    public float regenRate = 2f;
+    private float _elapsed;
+    public float regenDelay;
+    
+
+    private void Awake()
     {
-        
+        currentMana = maxMana;
+        _elapsed = 0f;
+        regenDelay = 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        RegenMana();
+        manaText.text = "Stamina: " + Decimal.Round((decimal)currentMana, 0);
+        manaBar.fillAmount = currentMana / 100;
+    }
+
+    public void RegenMana()
+    {
+        if (_elapsed > regenDelay)
+        {
+            currentMana = Mathf.Min(currentMana + regenRate * Time.deltaTime, maxMana);
+        }
+        _elapsed += Time.deltaTime;
+
+    }
+
+    public void LoseMana(float stamina)
+    {
+        _elapsed = 0f;
+        currentMana = Mathf.Max(currentMana - stamina, 0);
     }
 }
