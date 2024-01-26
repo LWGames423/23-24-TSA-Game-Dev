4b183597aa31fd54fa085728a5f4139fbd24dc81
DarkDwagon <shadowprince525@gmail.com>
2024-01-26 13:57:37 -0500

    added mouse hover and detecting clicking

---
+++ Assets/DetectMouseHover.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectMouseHover : MonoBehaviour
+{
+    public SpriteRenderer renderer;
+    public Sprite hoverSprite, regSprite;
+    public bool isOnSprite;
+
+    private void Awake()
+    {
+        renderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        renderer.sprite = hoverSprite;
+        isOnSprite = true;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.sprite = regSprite;
+        isOnSprite = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            if (isOnSprite)
+            {
+                Debug.Log("Pressed");
+            }
+        }
+    }
+}
