dd1c59ab701e548e48adea63def47cf140dd9e98
BryBryTheFry <bryanhtwang@gmail.com>
2024-01-28 18:06:20 -0500

    i broke 24 lol

--- Assets/_Scripts/RoomBehavior.cs
+++ Assets/_Scripts/RoomBehavior.cs
@@ -247,10 +247,9 @@ public class RoomBehavior : MonoBehaviour
         Debug.Log(room.roomType);
         if (room.roomType == "Minor Treasure" || room.roomType == "Key Room")
         {
+            Debug.Log(Random.Range(0, puzzles.Length));
             GameObject puzzle = Instantiate(puzzles[Random.Range(0, puzzles.Length)], transform.position + new Vector3(0.0f, 0.0f, 100.0f), Quaternion.identity, transform);
             puzzle.transform.parent = transform;
-
-            Debug.Log("Puzzle");
         }
     }
 
