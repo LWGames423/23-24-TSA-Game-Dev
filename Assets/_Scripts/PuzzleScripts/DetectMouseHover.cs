3919b0d0bfdcf46a13be3e473793287cb23127b9
DarkDwagon <shadowprince525@gmail.com>
2024-01-28 11:36:41 -0500

    Added some stuff and fixed some puzzles

--- Assets/_Scripts/PuzzleScripts/DetectMouseHover.cs
+++ Assets/_Scripts/PuzzleScripts/DetectMouseHover.cs
@@ -20,6 +20,7 @@ public class DetectMouseHover : MonoBehaviour
     public List<string> Chest2Strings = new List<string>() { "One chest tells the truth.", "At least one chest lies, and the treasure is not in Chest 3.", "This chest does not contain the treasure, however we all tell the truth.", "The other chests tell the truth, and the treasure is in Chest 3.", "The treasure is in this chest.", "This chest contains the treasure." };
     public List<string> Chest3Strings = new List<string>() { "The other chests tell the truth, and the treasure is not in Chest 1.", "Chest 1 tells the truth and the treasure is in his chest.", "One of the other chests lies.", "Both of the other chests lie, the treasure is in this chest.", "The treasure is not in this chest, and the others both lie.", "Both of the other chests tell the truth, and Chest 1 has the treasure." };
     public List<int> correctAnswers = new List<int>() { 1, 2, 2, 1, 1, 3 };
+    public bool hasCompleted = false;
 
     private void Start()
     {
@@ -42,14 +43,24 @@ public class DetectMouseHover : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(0))
+        {
             if (isOnSprite)
             {
-                if(isInstructions){
-                    PuzzleUI.SetActive(true);
+                if (isInstructions)
+                {
+                    if (!hasCompleted)
+                    {
+                        PuzzleUI.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.Log("you've already done this puzzle");
+                    }
                 }
-                if(isChest){
-                    if(thisChestNum == 1)
+                if (isChest)
+                {
+                    if (thisChestNum == 1)
                     {
                         ChestSpecificUI.GetComponentInChildren<TMP_Text>().text = Chest1Strings[problemNum];
                     }
@@ -78,14 +89,17 @@ public class DetectMouseHover : MonoBehaviour
 
     public void Chest1Func()
     {
-        if(correctChest == 1)
+        if (correctChest == 1)
         {
             PuzzleUI.SetActive(false);
             //add function here
+            hasCompleted = true;
         }
         else
         {
+            PuzzleUI.SetActive(false);
             Debug.Log("oopsie poopsie");
+            hasCompleted = true;
         }
     }
 
@@ -95,10 +109,13 @@ public class DetectMouseHover : MonoBehaviour
         {
             PuzzleUI.SetActive(false);
             //add function here
+            hasCompleted = true;
         }
         else
         {
+            PuzzleUI.SetActive(false);
             Debug.Log("oopsie poopsie");
+            hasCompleted = true;
         }
     }
 
@@ -108,10 +125,13 @@ public class DetectMouseHover : MonoBehaviour
         {
             PuzzleUI.SetActive(false);
             //add function here
+            hasCompleted = true;
         }
         else
         {
+            PuzzleUI.SetActive(false);
             Debug.Log("oopsie poopsie");
+            hasCompleted = true;
         }
     }
 }
