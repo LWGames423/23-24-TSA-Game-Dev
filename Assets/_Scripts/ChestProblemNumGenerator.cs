51e48b3acdc7bfb12666aa114ce2b1667cf17e08
DarkDwagon <shadowprince525@gmail.com>
2024-01-27 21:17:38 -0500

    Implemented the "Chest" puzzle

---
+++ Assets/_Scripts/ChestProblemNumGenerator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestProblemNumGenerator : MonoBehaviour
+{
+
+    System.Random rnd = new System.Random();
+    public int problemNum;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        problemNum = rnd.Next(0, 7);
+    }
+
+}
