bfec87800a8911e575bf0bed916bef706b379967
Joshua Lin <linzipper17@gmail.com>
2024-01-28 19:58:16 -0500

    SOUNDS SHIT

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip buttonSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            source.PlayOneShot(buttonSound);
+            Debug.Log("Click");
+        }
+    }
+}
