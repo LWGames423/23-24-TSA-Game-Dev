e31d2d1e4d09aed383bd99df5b6f543b355c24dc
BryBryTheFry <bryanhtwang@gmail.com>
2024-03-28 20:50:33 -0400

    more dio stuff

--- Assets/_Scripts/DialogueManager.cs
+++ Assets/_Scripts/DialogueManager.cs
@@ -7,6 +7,8 @@ public class DialogueManager : MonoBehaviour
 {
     [TextArea(3, 10)]
     public string[] text;
+    private string currentText;
+    private int currentTextID;
 
     public TMP_Text dialogueText;
     public GameObject dialoguePanel;
@@ -14,5 +16,39 @@ public class DialogueManager : MonoBehaviour
     public float timePerSection;
     public bool flex;
 
+    private void Awake()
+    {
+        currentTextID = 0;
+        currentText = text[currentTextID];
+        List<string> characters = new List<string>();
 
+        for (int i = 0; i < currentText.Length; i++)
+        {
+            characters.Add(currentText[i].ToString());
+            Debug.Log(characters[i]);
+        }
+    }
+
+    IEnumerator typeCharacter(string character, float time)
+    {
+        dialogueText.text += character;
+        yield return new WaitForSeconds(time);
+    }
+
+    public void FastForwardText()
+    {
+        dialogueText.text = currentText;
+        currentTextID++;
+        currentText = text[currentTextID];
+    }
+    
+    public void NextText()
+    {
+        if (text.Length - 1 > currentTextID)
+        {
+            dialogueText.text = "";
+            currentTextID++;
+            currentText = text[currentTextID];
+        }
+    }
 }
