9092b63461ca9f63e6b0ffd413f687543ceb2c55
Leo Wang <71103552+LWGames423@users.noreply.github.com>
2024-04-10 15:59:07 -0400

    pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _isActive = false;
+
+    public GameObject pauseMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !_isActive)
+        {
+            _isActive = true;
+        } else if(Input.GetKeyDown(KeyCode.Escape) && _isActive)
+        {
+            _isActive = false;
+        }
+
+        if (_isActive)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        
+        pauseMenu.SetActive(_isActive);
+    }
+
+    public void Resume()
+    {
+        _isActive = false;
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+}
